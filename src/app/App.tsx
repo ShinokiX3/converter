39e34da7da5ec42e7986427139d79257e612e32c
shinoki <endernasha85@gmail.com>
2023-12-04 22:25:08 +0300

    app structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import MainPage from '@pages/main/MainPage'
 import { Header } from '@widgets/header/Header'
+import cls from './App.module.scss';
+import { classNames } from '@/shared/lib/classNames/classNames';
 
 export const App = () => {
 
 	return (
-		<div>
+		<div className={classNames(cls.App, {}, [])}>
 			<Header />
-			<div>
+			<div className={cls.content_wrapper}>
+				{/* TODO: If we need more than one page, here the place, where we could put AppRouter */}
 				<MainPage />
 			</div>
 		</div>
