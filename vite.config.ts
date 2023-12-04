f63ce81ce41859ac2bd3a0be7516ffa093359802
shinoki <endernasha85@gmail.com>
2023-12-04 19:40:17 +0300

    paths resolves

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+			'@app': resolve(__dirname, './src/app/'),
+			'@assets': resolve(__dirname, './src/shared/assets/'),
+			'@shared': resolve(__dirname, './src/shared/'),
+			'@pages': resolve(__dirname, './src/pages/'),
+			'@widgets': resolve(__dirname, './src/widgets/'),
+		},
+	},
 })
