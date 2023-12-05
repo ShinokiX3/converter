cea501d627ff12f42bd67e1a7a3c63dcf2a184bd
shinoki <endernasha85@gmail.com>
2023-12-05 09:05:22 +0300

    new control icons

--- src/shared/assets/icons/controls/index.ts
+++ src/shared/assets/icons/controls/index.ts
@@ -1,3 +1,4 @@
 import exchange from './exchange.svg';
+import select from './select.svg';
 
-export const control_icons = { exchange };
+export const control_icons: { [key: string]: string } = { exchange, select };
