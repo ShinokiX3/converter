8bb45ef806591ce429f78ff7a2aa00c0b32337d5
shinoki <endernasha85@gmail.com>
2023-12-05 09:12:15 +0300

    main page ts lint fixes

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,36 +1,8 @@
-import { classNames } from '@/shared/lib/classNames/classNames';
-import { useEffect } from 'react';
+import { classNames } from '@shared/lib/classNames/classNames';
 import cls from './MainPage.module.scss';
-import { CurrencyRate } from '@/services';
-import { Converter } from '@/widgets/converter/Converter';
+import { Converter } from '@widgets/converter/Converter';
 
-const testData: CurrencyRate = {
-	"meta": {
-		"last_updated_at": "2023-12-03T23:59:59Z"
-	},
-	"data": {
-		"EUR": {
-			"code": "EUR",
-			"value": 0.0250327636
-		},
-		"GBP": {
-			"code": "GBP",
-			"value": 2.4678523447
-		},
-		"USD": {
-			"code": "USD",
-			"value": 0.0272640496
-		}
-	}
-}
 const MainPage = () => {
-
-	useEffect(() => {
-		(async () => {
-			// testData
-		})()
-	}, []);
-
 	return (
 		<div className={classNames(cls.MainPage, {}, [])}>
 			<Converter />
