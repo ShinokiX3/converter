b9ddadf021cf89bbe2154dfdd616adf3f9c11fad
shinoki <endernasha85@gmail.com>
2023-12-05 09:06:21 +0300

    final widgets variant

--- src/widgets/converter/Converter.tsx
+++ src/widgets/converter/Converter.tsx
@@ -1,37 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { classNames } from '@shared/lib/classNames/classNames';
 import cls from './Converter.module.scss';
 import { control_icons } from '@shared/assets/icons/controls';
 import { Input } from '@shared/ui/input/Input';
 import { Text } from '@shared/ui/text/Text';
-import { Select } from '@shared/ui/select/Select';
-import { Currency, CurrencyRate } from '@/services';
-import { flags } from '@/shared/assets/icons/flags';
-import { Button, ThemeButton } from '@/shared/ui/button/Button';
+import { Select, SelectOption } from '@shared/ui/select/Select';
+import { CurrencyRate, currencyApi } from '@/services';
+import { flags } from '@shared/assets/icons/flags';
+import { Button, ThemeButton } from '@shared/ui/button/Button';
+import { dateFormat } from '@shared/lib/dateFormat/dateFormat';
+import { CURRENCY_FULL_NAMES, CURRENCY_OPTIONS } from '@services/currency/const';
 
-const testData: CurrencyRate = {
-	"meta": {
-		"last_updated_at": "2023-12-03T23:59:59Z"
-	},
-	"data": {
-		"UAH": {
-			"code": "UAH",
-			"value": 0.0250327636
-		},
-		"EUR": {
-			"code": "EUR",
-			"value": 0.0250327636
-		},
-		"GBP": {
-			"code": "GBP",
-			"value": 2.4678523447
-		},
-		"USD": {
-			"code": "USD",
-			"value": 0.0272640496
-		}
-	}
-}
 
 interface CurrencyItemProps {
     value: string
@@ -41,7 +20,7 @@ const CurrencyItem = ({ value }: CurrencyItemProps) => {
 	return (
 		<div className={cls.currency_item}>
 			<img src={flags[value]} alt="" />
-			{value} - British Pound Sterling
+			{value} - {CURRENCY_FULL_NAMES[value].title}
 		</div>
 	)
 }
@@ -51,43 +30,126 @@ interface ConverterProps {
 }
 
 export const Converter = ({ className = '' }: ConverterProps) => {
-	const [amount, setAmount] = useState<string>('0');
-	console.log(Object.values(testData.data).map(({code}) => ({ value: code })));
-    
+	const [baseCurrency, setBaseCurrency] = useState<string>('');
+	const [exchangeCurrency, setExchangeCurrency] = useState<string>('');
+
+	const [totalTitle, setTotalTitle] = useState<string>('');
+	const [total, setTotal] = useState<string>('');
+
+	const [currencyRate, setCurrencyRate] = useState<CurrencyRate>();
+	
+	const [selectedBase, setSelectedBase] = useState<SelectOption>();
+	const [selectedCurrency, setSelectedCurrency] = useState<SelectOption>();
+
+	const baseCurrencyRef = useRef<HTMLInputElement | null>(null);
+	const exchangeCurrencyRef = useRef<HTMLInputElement | null>(null);
+
+	const ratesCollected = useMemo(() => {
+		if (currencyRate !== undefined) return dateFormat(new Date(currencyRate.meta.last_updated_at))
+	}, [currencyRate])
+
+	const value: number = useMemo(() => { 
+		if (currencyRate !== undefined) 
+			return currencyRate.data[Object.keys(currencyRate?.data)[0]].value; 
+		else return 1;
+	}, [currencyRate])
+	
+
+	useEffect(() => {
+		(async () => {
+			if (selectedBase && selectedCurrency) {
+				const base = selectedBase.value;
+				const currency = selectedCurrency.value;
+
+				const response = await currencyApi.fetchCurrencyRate([currency], base);
+				if (response !== null && response !== undefined && response.data) 
+					setCurrencyRate(response.data);
+			}
+		})()
+	}, [selectedBase, selectedCurrency]);
+
+	useEffect(() => {
+		if (currencyRate !== undefined) {
+			const total = (Number(baseCurrency) * value).toFixed(2);
+			
+			if (exchangeCurrencyRef.current) exchangeCurrencyRef.current.value = total;
+		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [baseCurrency, selectedBase]);
+
+	useEffect(() => {
+		if (currencyRate !== undefined) {
+			const total = (Number(exchangeCurrency) / value).toFixed(2);
+
+			if (baseCurrencyRef.current) baseCurrencyRef.current.value = total;
+		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [exchangeCurrency]);
+
+	const handleReverse = () => {
+		if (selectedBase && selectedCurrency) {
+			const temp = selectedBase;
+			setSelectedBase(selectedCurrency);
+			setSelectedCurrency(temp);
+		}
+	}
+
+	const handleBase = (value_: string) => {
+		setTotal(String(Number(value_) * value)); 
+		setBaseCurrency(value_);
+		setTotalTitle(value_);
+	}
+
+	const handleExchange = (value_: string) => {
+		setTotal(value_);
+		setExchangeCurrency(value_);
+		setTotalTitle(String(Number(value_) / value))
+	}
 
 	return (
 		<div className={classNames(cls.Converter, {}, [className])}>
 			<div className={cls.controls}>
 				<Select 
-					options={Object.values(testData.data).map(({code}) => ({ value: code }))} 
-					selected={{ value: 'EUR' }}
+					options={Object.values(CURRENCY_OPTIONS.data).map(({code}) => ({ value: code }))} 
+					selected={selectedBase}
+					onSelect={setSelectedBase}
 					RenderWith={CurrencyItem}
 				/>
-				<Button theme={ThemeButton.SO_CLEAR}>
+				<Button theme={ThemeButton.SO_CLEAR} onClick={handleReverse}>
 					<img src={control_icons.exchange} height={'25'} alt="" />
 				</Button>
 				<Select 
-					options={Object.values(testData.data).map(({code}) => ({ value: code }))} 
-					selected={{ value: 'EUR' }}
+					options={Object.values(CURRENCY_OPTIONS.data).map(({code}) => ({ value: code }))} 
+					selected={selectedCurrency}
+					onSelect={setSelectedCurrency}
 					RenderWith={CurrencyItem}
 				/>
 			</div>
 			<div className={cls.review}>
 				<Input 
+					ref={baseCurrencyRef}
 					type="text" 
-					header='Amount' 
-					value={amount} 
-					onChange={setAmount} 
+					header={`${selectedBase?.value || 'Base currency'}`} 
+					value={baseCurrency} 
+					onChange={handleBase} 
+					placeholder='input your amount'
+				/>
+				<Input 
+					ref={exchangeCurrencyRef}
+					type="text" 
+					header={`${selectedCurrency?.value || 'Exchange currency'}`} 
+					value={exchangeCurrency} 
+					onChange={handleExchange} 
 					placeholder='input your amount'
 				/>
 				<div className={cls.result}>
 					<Text 
-						title="12 USD"
-						text='654.12 ETB'
+						title={`${Number(totalTitle)?.toFixed(3)} ${selectedBase?.value || ''}`}
+						text={`${Number(total)?.toFixed(3) || baseCurrency} ${selectedCurrency?.value || ''}`}
 						className={classNames('', {}, [cls.to_end, cls.values])} 
 					/>
 					<Text 
-						text='Market rates'
+						text={`Market rates collected - ${ratesCollected || '...'}`}
 						className={classNames('', {}, [cls.to_end, cls.date])} 
 					/>
 				</div>
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,55 +1,77 @@
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { useState } from 'react';
+import { classNames } from '@shared/lib/classNames/classNames';
 import cls from './header.module.scss';
-
 import { flags } from '@assets/icons/flags';
 import { CurrencyRate } from '@/services';
 import { Currency, CurrencyTheme } from '@shared/ui/currency/Currency';
+import { Button, ThemeButton } from '@shared/ui/button/Button';
+import { Portal } from '@shared/ui/portal/Portal';
+import { CurrencyData } from '@services/currency/types';
+import { Loader } from '@/shared/ui/loader/Loader';
 
-const testData: CurrencyRate = {
-	"meta": {
-		"last_updated_at": "2023-12-03T23:59:59Z"
-	},
-	"data": {
-		"EUR": {
-			"code": "EUR",
-			"value": 0.0250327636
-		},
-		"GBP": {
-			"code": "GBP",
-			"value": 2.4678523447
-		},
-		"USD": {
-			"code": "USD",
-			"value": 0.0272640496
-		}
-	}
+interface LogoProps {
+	title: string
 }
 
-const Logo = () => {
+const Logo: React.FC<LogoProps> = ({ title }) => {
 	return (
 		<div className={cls.logo}>
 			<img src={flags.UAH} alt="" />
-			<p>Currency Rate Review</p>
+			<p>{title}</p>
 		</div>
 	)
 }
 
-const Rates = () => {
+interface RatesProps {
+	data: CurrencyData;
+}
+
+const Rates: React.FC<RatesProps> = ({ data }) => {
 	return (
 		<div className={cls.rates}>
-			{Object.values(testData.data).map(data => (
+			{Object.values(data).map(data => (
 				<Currency key={data.code} data={data} theme={CurrencyTheme.ROUNDED} />
 			))}
 		</div>
 	)
 }
 
+interface HeaderProps {
+	currencyRate: CurrencyRate | null;
+}
 
-export const Header = () => {
+export const Header: React.FC<HeaderProps> = ({ currencyRate }) => {
+	const [menuVisible, setMenuVisible] = useState<boolean>(false);
+
 	return (
-		<header className={classNames(cls.Header, {}, [])}>
-			<Logo />
-			<Rates />
-		</header>
+		<>
+			<header className={classNames(cls.Header, {}, [])}>
+				<Logo title='Currency Rate Review' />
+				{currencyRate ? <Rates data={currencyRate.data} /> : <Loader />}
+				<div className={cls.mobile}>
+					<Button 
+						className={cls.mobile_button} 
+						theme={ThemeButton.OUTLINE}
+						onClick={() => setMenuVisible(!menuVisible)}
+					>
+						Show rates for UAH
+					</Button>
+				</div>
+			</header>
+			{menuVisible ? 
+				<Portal>
+					<div className={cls.mobile_menu}>
+						<Button 
+							className={cls.mobile_button} 
+							theme={ThemeButton.OUTLINE}
+							onClick={() => setMenuVisible(!menuVisible)}
+						>
+							Close
+						</Button>
+						{currencyRate ? <Rates data={currencyRate.data} /> : <div>Loading</div>}
+					</div>
+				</Portal> 
+				: null}
+		</>
 	);
 };
